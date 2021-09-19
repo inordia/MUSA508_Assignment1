b05bc735b26181200c66342dd824ccbde2e916f5
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2021-09-19 10:24:37 -0400

    Add files via upload

--- Census data.R
+++ Census data.R
@@ -10,21 +10,10 @@ options(tigris_class = "sf")
 #Census API Key
 census_api_key("744a6c627b7cc93310688f5ce408189636167475", install = TRUE)
 #Get Chicago boundary
-CTA_boundary <-st_read("https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=KML") %>%
+CTA_boundary <-st_read("https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=KML") %>%
   select(-Description)%>%
   st_transform('EPSG:26916')
 View(CTA_boundary)
-
-
-Chicago_boundary <- st_read("https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=KML")%>%
-  select(-Name, -Description)%>%
-  st_transform('EPSG:26916')
-
-Chicago00 <- st_intersection(Chicago_boundary, tracts00)
-
-ggplot() +
-  geom_sf(data = Chicago00, aes(fill = value)) +
-  theme(plot.title = element_text(size=22))
 
 # ---- Year 2000 tracts -----
 vars00 <- c("P001001","P006002","PCT025050","PCT025009","P053001","H056001","P092001")
@@ -35,15 +24,15 @@ tracts00 <-
 View(tracts00)
 
 #Clip the boundary 
-
 clip00 <- 
-  st_intersection(tracts00, CTA_boundary) %>%
-  dplyr::select(value,variable)
+  st_intersection(tracts00,CTA_boundary) %>%
+  dplyr::select(value,variable) 
+plot(clip00)
+View(clip00)
 
 #Convert data to the wide format, rename the variables and recreate new variables
 tracts00<- 
   clip00 %>% 
-  dplyr::select(-Selection_Type)%>% 
   spread(variable, value)%>% 
   rename (
     TotalPop = P001001,
@@ -58,6 +47,8 @@ tracts00<-
          pctPoverty = ifelse(TotalPop > 0, TotalPoverty / TotalPop, 0),
          year = "2000") %>%
   dplyr::select(-Whites,-FemaleBachelors,-MaleBachelors,-TotalPoverty)
+View(tracts00)
+
 
 # ---- Year 2017 tracts -----
 #get and clip 2017 data 
@@ -72,8 +63,9 @@ glimpse(tracts17)
 
 #Clip the boundary 
 clip17 <- 
-  st_intersection(tracts17, CTA_boundary) %>%
+  st_intersection(CTA_boundary,tracts17) %>%
   dplyr::select(variable,estimate) 
+plot(clip17)
 
 #Convert data to the wide format, rename the variables and recreate new variables
 tracts17<- 
@@ -93,6 +85,7 @@ tracts17<-
   dplyr::select(-Whites, -FemaleBachelors, -MaleBachelors, -TotalPoverty) 
 View(tracts17)
 
+
 # --- Combining 09 and 17 data ----
 
 allTracts <- rbind(tracts00,tracts17)
@@ -100,3 +93,4 @@ dim(tracts00)
 dim(tracts17)
 dim(allTracts)
 View(allTracts)
+
