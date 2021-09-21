b14bda61f97aef241c5bb2b701967f041c961d85
inordia <rw1932@nyu.edu>
2021-09-21 19:32:27 -0400

    boundary_buffer

--- Assignment1_draft.R
+++ Assignment1_draft.R
@@ -5,6 +5,9 @@ library(tidyverse)
 library(tidycensus)
 library(sf)
 library(kableExtra)
+library(mapview)
+
+
 
 options(scipen=999)
 options(tigris_class = "sf")
@@ -48,8 +51,6 @@ plotTheme <- function(base_size = 12) {
   )
 }
 
-?round
-
 palette5 <- c("#ffffcc","#a1dab4","#41b6c4","#2c7fb8","#253494")
 
 qBr <- function(df, variable, rnd) {
@@ -57,37 +58,29 @@ qBr <- function(df, variable, rnd) {
     as.character(quantile(round(df[[variable]],0),
                           c(.01,.2,.4,.6,.8), na.rm=T))
   } else if (rnd == FALSE | rnd == F) {
-    as.character(formatC(quantile(df[[variable]]), digits = 3),
-                 c(.01,.2,.4,.6,.8), na.rm=T)
+    as.character(formatC(quantile(df[[variable]],
+                                  c(.01,.2,.4,.6,.8), na.rm=T), digits = 3))
   }
 }
+
 
 q5 <- function(variable) {as.factor(ntile(variable, 5))}
 
 #Chicago boundaries
 
 CTA_boundary <-st_read("https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=KML") %>%
-  select(-Name, -Description)%>%
   st_transform('EPSG:26916')
+CTA_boundary_buffer <- st_union(CTA_boundary)
 
 vars00 <- c("P001001","P006002","PCT025050","PCT025009","P053001","H056001","P092001")
 tracts00 <-  
   get_decennial(geography = "tract", vars00, 
                 year=2000, state='IL', county='Cook', geometry=T) %>%  
   st_transform('EPSG:26916')
-View(tracts00)
 
-#Clip the boundary 
-clip00 <- 
-  st_intersection(CTA_boundary,tracts00) %>%
-  dplyr::select(GEOID,value,variable) 
-plot(clip00)
-View(clip00)
 
 #Convert data to the wide format, rename the variables and recreate new variables
-tracts00<- 
-  clip00 %>% 
-  spread(variable, value)%>% 
+tracts00<- tracts00%>%spread(variable, value)%>% 
   rename (
     TotalPop = P001001,
     Whites = P006002,
@@ -103,9 +96,12 @@ tracts00<-
   dplyr::select(-Whites,-FemaleBachelors,-MaleBachelors,-TotalPoverty)
 View(tracts00)
 
+#Clip the boundary 
+tracts00 <- st_intersection(tracts00,CTA_boundary_buffer)
+
 
 # ---- Year 2017 tracts -----
-#get and clip 2017 data 
+
 vars17 <- c("B25026_001E","B02001_002E","B15001_050E",
             "B15001_009E","B19013_001E","B25058_001E",
             "B06012_002E")
@@ -113,13 +109,13 @@ tracts17 <-
   get_acs(geography = "tract", vars17, 
           year=2017, state='IL', county='Cook', geometry=T) %>%
   st_transform('EPSG:26916')
-glimpse(tracts17)
+#glimpse(tracts17)
 
 #Clip the boundary 
 clip17 <- 
-  st_intersection(tracts17,CTA_boundary) %>%
+  st_intersection(tracts17,CTA_boundary_buffer) %>%
   dplyr::select(GEOID,variable,estimate) 
-plot(clip17)
+#plot(clip17)
 
 #Convert data to the wide format, rename the variables and recreate new variables
 tracts17<- 
@@ -136,8 +132,7 @@ tracts17<-
          pctBachelors = ifelse(TotalPop > 0, ((FemaleBachelors + MaleBachelors) / TotalPop),0),
          pctPoverty = ifelse(TotalPop > 0, TotalPoverty / TotalPop, 0),
          year = "2017") %>%
-  dplyr::select(-Whites, -FemaleBachelors, -MaleBachelors, -TotalPoverty) 
-View(tracts17)
+  dplyr::select(-Whites, -FemaleBachelors, -MaleBachelors, -TotalPoverty)
 
 #Binding
 
@@ -152,8 +147,9 @@ tractsboundary <-st_union(tracts17)
 
 CTA_stops <- st_read("https://data.cityofchicago.org/download/4qtv-9w43/application%2Fxml") %>%
   select(-Description)%>%
-  st_transform('EPSG:26916')%>%
-  CTA_stops[tractsboundary,]
+  st_transform('EPSG:26916')
+
+CTA_stops<- CTA_stops[tractsboundary,]
 
 plot(CTA_stops)
 
@@ -259,5 +255,94 @@ ggplot(allTracts.group)+
   labs(title = "Percent of Poverty 2000-2017", subtitle = "The red border indicates areas within 1/2 mile to transit stations. ") +
   mapTheme() + theme(plot.title = element_text(size=22))
 
-#Table
+#TOD Indicators Table
+
+allTracts.Summary <- 
+  st_drop_geometry(allTracts.group) %>%
+  group_by(year, TOD) %>%
+  summarize(Rent = mean(MedRent, na.rm = T),
+            Population = mean(TotalPop, na.rm = T),
+            Percent_White = mean(pctWhite, na.rm = T),
+            Percent_Bach = mean(pctBachelors, na.rm = T),
+            Percent_Poverty = mean(pctPoverty, na.rm = T))
+
+allTracts.Summary %>%
+  unite(year.TOD, year, TOD, sep = ": ", remove = T) %>%
+  gather(Variable, Value, -year.TOD) %>%
+  mutate(Value = round(Value, 2)) %>%
+  spread(year.TOD, Value) %>%
+  kable() %>%
+  kable_styling() %>%
+  footnote(general_title = "TOD Inditators, Chicago",
+           general = "\n")
+
+#TOD Indicators Plots
+
+allTracts.Summary %>%
+  gather(Variable, Value, -year, -TOD) %>%
+  ggplot(aes(year, Value, fill = TOD)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  facet_wrap(~Variable, scales = "free", ncol=5) +
+  scale_fill_manual(values = c("#bae4bc", "#0868ac")) +
+  labs(title = "Indicator differences across time and space") +
+  plotTheme() + theme(legend.position="bottom")
+
+#sub-markets
+
+Loop <- c (17031320100,	
+           17031320200,
+           17031320300,
+           17031320400,	
+           17031320500,	
+           17031320600)
+
+Downtown <- subset(allTracts.group, allTracts$GEOID==Loop)%>%
+  mutate(submarket = "downtown")
+
+#Graduated Symbol Maps
+ggplot()+
+  geom_sf(data = tracts17,
+          fill = "white", color = "grey75")+
+  geom_sf(data = tracts17%>%
+            st_centroid(),
+          shape = 21, color='grey10',
+          aes(
+            size = TotalPop,
+            fill = TotalPop
+          )) +
+  scale_size_continuous(
+    range = c(0,5),
+    breaks = c(0,2002,3050,4536,18841), 
+    labels = qBr(tracts17, "TotalPop"),
+    name="Total Population"
+  ) +
+  scale_fill_stepsn(	
+    colors = palette5,
+    breaks = c(0,2002,3050,4536,18841),
+    guide = FALSE
+  )+
+  labs(title = "Total Population") +
+  theme(axis.title=element_blank(),
+        axis.text=element_blank(), axis.ticks=element_blank(), # remove ticks
+        panel.background = element_rect(fill='gray'))+
+  guides(
+    size = guide_legend(
+      override.aes = list(fill = palette5)
+    ))
+
+#Rent and Distance to Subway Station
+
+multipleRingBuffer <- function(inputPolygon, maxDistance, interval) 
+
+allTracts.rings <-
+  st_join(st_centroid(dplyr::select(allTracts, GEOID, year)), 
+          multipleRingBuffer(buffer, 15000, 2640)) %>%
+  st_drop_geometry() %>%
+  left_join(dplyr::select(allTracts, GEOID, MedRent, year), 
+            by=c("GEOID"="GEOID", "year"="year")) %>%
+  st_sf() %>%
+  mutate(distance = distance*0.00062)
+
+#Crime Data
+cime <- st_read("https://data.cityofchicago.org/api/views/d62x-nvdr/rows.xml?accessType=DOWNLOAD")
 
